81c53762764cef85f80cb27457181c5e01468b85
cascrielflexso <121176892+cascrielflexso@users.noreply.github.com>
2022-12-26 13:01:43 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,29 +1,44 @@
-(function () {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = "<button>hello</button>";   
-   
-    class PerformanceHelp extends HTMLElement {
-        constructor() {
-            super();
-            this.init();           
-        }
-        init() {            
-            let shadowRoot = this.attachShadow({mode: "open"});
-            shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this.addEventListener("click", event => {
-            var event = new Event("onClick");
-            this.fireChanged();           
-            this.dispatchEvent(event);
-            });           
-        }
+(function() { 
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<style>
+		:host {
+			border-radius: 25px;
+			border-width: 4px;
+			border-color: black;
+			border-style: solid;
+			display: block;
+		} 
+		</style> 
+	`;
 
+	class PerformanceHelp extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+                this.fireChanged()
+				this.dispatchEvent(event);
+			});
+			this._props = {};
+		}
         fireChanged() {
             const dataBinding = this.dataBindings.getDataBinding('myDataBinding')
             alert(`Dimensions : ${dataBinding.getDimensions('dimensions').toString()}`);     
            
         }        
-        
-    }
 
-    customElements.define('custom-button', PerformanceHelp);
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("prod_number" in changedProperties) {
+				this.prod_number = changedProperties["prod_number"];
+			}
+		}
+	}
+	customElements.define("custom-button", PerformanceHelp);
 })();
