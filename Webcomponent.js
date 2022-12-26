32da688bc08acc6ef63e119d1a83f531f2acabd5
cascrielflexso <121176892+cascrielflexso@users.noreply.github.com>
2022-12-26 15:58:05 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,18 +1,26 @@
 (function() { 
 	let template = document.createElement("template");
 	template.innerHTML = `
-		<button>test</button>
+		<style>
+		:host {
+			border-radius: 25px;
+			border-width: 4px;
+			border-color: black;
+			border-style: solid;
+			display: block;
+		} 
+		</style> 
 	`;
 
 	class ColoredBox extends HTMLElement {
 		constructor() {
-		super(); 
-		let shadowRoot = this.attachShadow({mode: "open"});
-		shadowRoot.appendChild(template.content.cloneNode(true));
-		this.addEventListener("click", event => {
-			var event = new Event("onClick");
-			this.dispatchEvent(event);
-		});
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
 			this._props = {};
 		}
 
@@ -24,10 +32,8 @@
 			if ("color" in changedProperties) {
 				this.style["background-color"] = changedProperties["color"];
 			}
-			/*if ("prod_number" in changedProperties) {
-				this.prod_number = changedProperties["prod_number"];
-			}*/
 		}
 	}
+
 	customElements.define("custom-button", ColoredBox);
 })();
