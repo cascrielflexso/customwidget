4066054e40c9727ea2db5bc9f3c5e187e43f4e69
cascrielflexso <121176892+cascrielflexso@users.noreply.github.com>
2022-12-26 15:37:02 +0100

    Update builderpanel.js

--- builderpanel.js
+++ builderpanel.js
@@ -9,15 +9,6 @@
 						<td>Color</td>
 						<td><input id="builder_color" type="text" size="40" maxlength="40"></td>
 					</tr>
-					<tr>
-						<td>Product</td>
-						<td><select id="products"></select>
-						</td>
-					</tr>
-					<tr>
-						<td>Color</td>
-						<td><input id="builder_color" type="text" size="40" maxlength="40"></td>
-					</tr>
 				</table>
 				<input type="submit" style="display:none;">
 			</fieldset>
@@ -30,19 +21,6 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
 			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-			const dataBinding = this.dataBindings.getDataBinding('myDataBinding')
-			
-			
-			const products = this._shadowRoot.getElementById("countriesDropDown");
-			for (let key in dataBinding.getMembers("Product")) {
- 		 		let option = document.createElement("option");
-  				option.setAttribute('value', data[key]);
-
-  				let optionText = document.createTextNode(key);
-  				option.appendChild(optionText);
-
-			  	products.appendChild(option);
-		}
 		}
 
 		_submit(e) {
@@ -63,14 +41,6 @@
 
 		get color() {
 			return this._shadowRoot.getElementById("builder_color").value;
-		}
-		
-		set prod_number(newProd_number) {
-			this._shadowRoot.getElementById("products").options[this._shadowRoot.getElementById("products").selectedIndex].text = newProd_number;
-		}
-
-		get prod_number() {
-			return this._shadowRoot.getElementById("products").value;
 		}
 	}
 
