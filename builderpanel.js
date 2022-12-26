8c1069eb695f4bfde523ce7f85219474ee84084d
cascrielflexso <121176892+cascrielflexso@users.noreply.github.com>
2022-12-26 13:08:23 +0100

    Update builderpanel.js

--- builderpanel.js
+++ builderpanel.js
@@ -1,13 +1,32 @@
 (function()  {
 	let template = document.createElement("template");
-	template.innerHTML = `<p>hello</p>`;
+	template.innerHTML = `
+		<form id="form">
+			<fieldset>
+				<legend>Properties</legend>
+				<table>
+					<tr>
+						<td>Product Number</td>
+						<td><input id="builder_prod_number" type="text" size="15" maxlength="10"></td>
+					</tr>
+				</table>
+				<input type="submit" style="display:none;">
+			</fieldset>
+		</form>
+		<style>
+		:host {
+			display: block;
+			padding: 1em 1em 1em 1em;
+		}
+		</style>
+	`;
 
 	class BoxBps extends HTMLElement {
 		constructor() {
 			super();
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			//this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
 		}
 
 		_submit(e) {
@@ -21,12 +40,12 @@
 			}));
 		}
 
-		set prod_number(prod_number) {
-			//this._shadowRoot.getElementById("prod_number").value = prod_number;
+		set prod_number(new_prod_number) {
+			this._shadowRoot.getElementById("builder_prod_number").value = new_prod_number;
 		}
 
 		get prod_number() {
-			//return this._shadowRoot.getElementById("prod_number").value;
+			return this._shadowRoot.getElementById("builder_prod_number").value;
 		}
 	}*/
 
