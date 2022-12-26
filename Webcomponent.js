a4bb996e2d1ddecb33c6954f92dbc550aec7d54f
cascrielflexso <121176892+cascrielflexso@users.noreply.github.com>
2022-12-26 17:42:25 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,6 +1,6 @@
 (function() { 
 	let template = document.createElement("template");
-	const dataBinding = this.dataBindings.myDataBinding
+	const dataBinding = this.myDataBinding
 	//let prd = dataBinding.data["Product"][this.prod_number]
 	console.log(dataBinding.data.toString())
 	template.innerHTML = `
